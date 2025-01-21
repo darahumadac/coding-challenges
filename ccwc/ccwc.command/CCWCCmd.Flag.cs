627a4df4ac31cef5a9ee080950129768be1514a1
darah <darahfumadac@gmail.com>
2025-01-22 00:16:41 +0800

    fix byte count flag result

--- ccwc/ccwc.command/CCWCCmd.Flag.cs
+++ ccwc/ccwc.command/CCWCCmd.Flag.cs
@@ -32,7 +32,18 @@ public partial class CCWCCmd
     {
         public void Execute(ICommand command)
         {
-            command.AppendResult(Encoding.UTF8.GetByteCount(command.GetData()).ToString());
+            Encoding utf8 = Encoding.UTF8;
+            /*
+            - To achieve same byte count with wc -c in linux, must make sure to add the BOM (Byte Order Mark).
+            - A byte order mark (BOM) is a sequence of bytes that indicates the encoding and byte order of a text file. 
+                It's a Unicode character that's usually placed at the beginning of a file.
+            - The BOM tells software how to interpret the bytes in a file.
+            - C# GetByteCount does not count the byte order mark because the BOM is not part of the encoded characters.
+                However, Unix/Linux counts the BOM as part of the byte count. So, we need to get the BOM length and add it
+                to GetByteCount result.
+            */
+            int bomByteCount = utf8.GetPreamble().Length;
+            command.AppendResult((utf8.GetByteCount(command.GetData()) + bomByteCount).ToString());
         }
     }
 }
